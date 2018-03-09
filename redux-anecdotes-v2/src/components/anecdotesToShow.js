73f7dc099860c99b711b55f030bb8c870f98762d
Janne <janne@janne-thinkpad.localdomain>
2018-03-09 19:51:24 +0200

    pieni muutos siisteyden vuoksi

--- redux-anecdotes-v2/src/components/anecdotesToShow.js
+++ redux-anecdotes-v2/src/components/anecdotesToShow.js
@@ -1,5 +1,5 @@
 const anecdoteToShow = ({anecdote, filter}) => {
-  return filter === '' ? anecdote : anecdote.filter(a => a.content.toLowerCase().includes(filter.toLowerCase()))
+  return !filter ? anecdote : anecdote.filter(a => a.content.toLowerCase().includes(filter.toLowerCase()))
 }
 
 export default anecdoteToShow
